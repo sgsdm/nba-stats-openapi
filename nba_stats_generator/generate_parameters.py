d9b6b7e7cd1553e5e718dc55d762fdcb8f512f8f
Abhi Agarwal <abhiaagarwal01@gmail.com>
2024-01-31 19:03:10 -0500

    Fix generation script

--- nba_stats_generator/generate_parameters.py
+++ nba_stats_generator/generate_parameters.py
@@ -33,6 +33,7 @@ def generate_parameters(dir: pathlib.Path) -> None:
 
     parameters: set[str] = set()
     data_sets: set[str] = set()
+    schemas: set[str] = set()
     openapi["paths"] = {}
     for endpoint, endpoint_vals in analysis.items():
         if endpoint_vals["status"] in ["deprecated", "invalid"]:
@@ -66,14 +67,14 @@ def generate_parameters(dir: pathlib.Path) -> None:
                                         "resource": {"const": endpoint.lower()},
                                         "resultSets": {
                                             "type": "array",
-                                            "items": [],
+                                            "prefixItems": [],
                                         },
                                     },
                                 }
                             },
                         },
-                        "404": {"description": "[to do]"},
                     },
+                    "404": {"description": "[to do]"},
                 },
             }
         }
@@ -104,17 +105,21 @@ def generate_parameters(dir: pathlib.Path) -> None:
             with (parameter_dir / f"{parameter}.json").open("w") as param_file:
                 json.dump(temp_param, param_file, indent=2)
 
+            schema = parameter
+            if schema in schemas:
+                continue
+            else:
+                schemas.add(schema)
+
             temp_schema: dict[str, Any] = {"description": "[to do]"}
-            if parameter in endpoint_vals["nullable_parameters"]:
+            if schema in endpoint_vals["nullable_parameters"]:
                 temp_schema["type"] = [None, "string"]
                 nullable = True
             else:
                 temp_schema["type"] = "string"
                 nullable = False
 
-            if (
-                pattern := endpoint_vals["parameter_patterns"].get(parameter)
-            ) is not None:
+            if (pattern := endpoint_vals["parameter_patterns"].get(schema)) is not None:
                 if "{" in pattern:  # probably regex. probably.
                     temp_schema["pattern"] = pattern
                 else:
@@ -125,15 +130,22 @@ def generate_parameters(dir: pathlib.Path) -> None:
                     if nullable:
                         temp_schema["enum"].insert(0, None)
 
-            with (schema_dir / f"{parameter}.json").open("w") as schema_file:
+            with (schema_dir / f"{schema}.json").open("w") as schema_file:
                 json.dump(temp_schema, schema_file, indent=2)
+
+        temp_data_sets = endpoint_vals["data_sets"]
+        temp_path["get"]["responses"]["200"]["content"]["application/json"]["schema"][
+            "properties"
+        ]["resultSets"]["prefixItems"] = [
+            {"$ref": f"../data_sets/{data_set}.json"} for data_set in temp_data_sets
+        ]
 
         temp_path["get"]["responses"]["200"]["content"]["application/json"]["schema"][
             "properties"
-        ]["resultSets"]["items"] = [
-            {"type": {"$ref": f"../data_sets/{data_set}.json"}}
-            for data_set in endpoint_vals["data_sets"]
-        ]
+        ]["resultSets"]["minItems"] = len(temp_data_sets)
+        temp_path["get"]["responses"]["200"]["content"]["application/json"]["schema"][
+            "properties"
+        ]["resultSets"]["maxItems"] = len(temp_data_sets)
 
         with (path_dir / f"{endpoint}.json").open("w") as path_file:
             json.dump(temp_path, path_file, indent=2)
@@ -164,11 +176,13 @@ def generate_parameters(dir: pathlib.Path) -> None:
                     "name": {"type": "string"},
                     "headers": {
                         "type": "array",
-                        "items": [
+                        "prefixItems": [
                             {"const": data_set_val} for data_set_val in data_set_vals
                         ],
+                        "minItems": len(data_set_vals),
+                        "maxItems": len(data_set_vals),
                     },
-                    "rowSet": {"type": "array", "items": []},
+                    "rowSet": {"type": "array", "prefixItems": []},
                 },
             }
 
@@ -177,14 +191,14 @@ def generate_parameters(dir: pathlib.Path) -> None:
                     [val.capitalize() for val in column_val.split("_")]
                 )
 
-                temp_data_set["properties"]["rowSet"]["items"].append(
-                    {"type": {"$ref": f"../schemas/{cleaned_column_val}.json"}}
+                temp_data_set["properties"]["rowSet"]["prefixItems"].append(
+                    {"$ref": f"../schemas/{cleaned_column_val}.json"}
                 )
 
-                if cleaned_column_val in parameters:
+                if cleaned_column_val in schemas:
                     continue
                 else:
-                    parameters.add(cleaned_column_val)
+                    schemas.add(cleaned_column_val)
 
                 with (schema_dir / f"{cleaned_column_val}.json").open(
                     "w"
@@ -198,26 +212,24 @@ def generate_parameters(dir: pathlib.Path) -> None:
             with (data_set_dir / f"{data_set}.json").open("w") as data_set_file:
                 json.dump(temp_data_set, data_set_file, indent=2)
 
-    for openapi_dir in [schema_dir, parameter_dir, data_set_dir]:
+    for openapi_dir, items in {schema_dir: schemas, parameter_dir: parameters}.items():
         with (openapi_dir / "_index.json").open("w") as index_schema_file:
             json.dump(
-                {
-                    parameter: {"$ref": f"./{parameter}.json"}
-                    for parameter in sorted(parameters)
-                },
+                {item: {"$ref": f"./{item}.json"} for item in sorted(items)},
                 index_schema_file,
                 indent=2,
             )
 
-    with openapi_file_path.open("w") as openapi_file:
-        openapi.update(
-            {
-                "components": {
-                    "parameters": {"$ref": "specification/parameters/_index.json"},
-                    "schemas": {"$ref": "specification/schemas/_index.json"},
-                }
+    openapi.update(
+        {
+            "components": {
+                "parameters": {"$ref": "specification/parameters/_index.json"},
+                "schemas": {"$ref": "specification/schemas/_index.json"},
             }
-        )
+        }
+    )
+
+    with openapi_file_path.open("w") as openapi_file:
         json.dump(openapi, openapi_file, indent=2)
 
     return
